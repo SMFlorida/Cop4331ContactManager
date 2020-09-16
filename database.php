82136677387afe346342995d714ac5b6da63076e
SMFlorida <54723276+SMFlorida@users.noreply.github.com>
2020-09-15 21:28:15 -0400

    initial api commit

---
+++ database.php
@@ -0,0 +1,26 @@
+<?php
+// used to get mysql database connection
+class Database{
+ 
+    // specify your own database credentials
+    private $host = "http://spadecontactmanager.com";
+    private $db_name = "cop4311g_shelbydatabase";
+    private $username = "cop4311g_shelbyuser";
+    private $password = "$hip$hape!";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
